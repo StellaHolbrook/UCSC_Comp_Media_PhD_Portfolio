411228184af1b52a9e1bf6edca00a0d0cdc893a9
jackholbrook <jholbrook@seattleu.edu>
2023-01-11 20:59:15 -0800

    added usgs and ucsb projects

---
+++ USGS_Stream_Termperature_Project/_targets.R
@@ -0,0 +1,11 @@
+library(targets)
+library(tarchetypes)
+
+# source two seperate targets files:
+source("1_fetch.R")
+source("2_map.R")
+source("3_summarize.R")
+
+c(fetch_targest_list, map_targets_list, meta_summaries)
+#end
+
